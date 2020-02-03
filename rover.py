3ace4f16d70c36cca8000858400ea5448c2f2130
oliviamiranda <your-email-address>
2020-02-03 18:05:29 -0500

    Lab 2

--- rover.py
+++ rover.py
@@ -1,4 +1,4 @@
 # time is distance over velocity
 time = 34000000 / 186000
-print ("the time that it takes a photo from Curiosity to reach NASA is", time, "seconds")
+print ("the time that it takes a photo from Curiosity to reach NASA is", time, "seconds.")
 
