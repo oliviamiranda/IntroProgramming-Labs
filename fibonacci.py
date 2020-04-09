7d7fcfd2185a1ca6bb3e9248e703cd21623a4cb1
oliviamiranda <your-email-address>
2020-04-09 13:46:48 -0400

    Lab 3 - Wrote an interactive Fibonacci Calculator

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,10 @@
-def main():
-    num = eval(input("Program to find the fibonacci number we want. \n What number you need"))
-    total = round (num/2)
-    x,y = 1,1
-    fi = list ([1,1])
-    for i in range(total):
-        x = x + y
-        fi. append(x)
-        print(fi[num-1])
-main()
+def Fibonacci(n):
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return Fibonacci(n - 1) + Fibonacci(n - 2)
+print(Fibonacci(6))
